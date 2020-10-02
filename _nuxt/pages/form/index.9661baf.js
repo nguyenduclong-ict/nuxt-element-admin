217ae421bca6ef6af1ff5483d779b12f0571c534
Nguyen Duc Long <nguyenduclong.ict@gmail.com>
2020-10-02 16:21:14 +0700

    Updates

---
+++ _nuxt/pages/form/index.9661baf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{790:function(e,n,t){"use strict";t.r(n);var c={},r=t(3),component=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"290f06c0",null);n.default=component.exports}}]);
---
+++ _nuxt/pages/profile.398260a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{792:function(e,n,t){"use strict";t.r(n);var c={},r=t(3),component=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"13f4cbe0",null);n.default=component.exports}}]);
